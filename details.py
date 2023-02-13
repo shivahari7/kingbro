197aa8210f879ef00cfa4a5d86a812defa4de31a
sanjay <124660178+shivahari7@users.noreply.github.com>
2023-02-13 22:16:07 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 20937540
 api_hash = "bf35fcc80dcea68066ec08b588cde18d"
-bot_token = "6054706338:AAEvWNFwzascy0oMwcl8Pe2o1nlGO1kwLVU"
+bot_token = "6272284277:AAHReE0n2g0mjuebC-HUuZJDraCHTtgbJVo"
 
